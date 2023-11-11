e77c90e50ff8264bee50aa93559124e2ce0c3438
johnklima <klima.de.sa@gmail.com>
2023-11-11 12:44:52 +0100

    Assign Materials

---
+++ Assets/Scripts/AssignMaterial.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignMaterial : MonoBehaviour
+{
+
+    public Material[] materials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 0;
+
+        foreach(Transform child in transform)
+        {
+           MeshRenderer mr;
+           if (child.TryGetComponent<MeshRenderer>(out mr))
+           {
+                mr.material = materials[i];
+                i++;
+                
+                if (i >= materials.Length)
+                    i = 0;
+           }
+        }
+    
+    }
+
+    
+}
